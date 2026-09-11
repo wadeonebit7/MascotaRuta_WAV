42985d614e23e05896c52f018fbe2b23af1d8ee8
Neiza Funes <neizafunes@gmail.com>
2026-09-11 12:09:22 -0300

    View main activity

--- app/src/main/java/com/example/mascotaruta_wav/ConfirmacionActivity.java
+++ app/src/main/java/com/example/mascotaruta_wav/ConfirmacionActivity.java
@@ -1,8 +1,6 @@
 package com.example.mascotaruta_wav;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ConfirmacionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_confirmacion);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        Button btn = findViewById(R.id.btnTutor);
-        btn.setOnClickListener(v -> {
-           Intent intent = new Intent(MainActivity.this, RegistrarMascotaActivity.class);
-           startActivity(intent);
         });
     }
 }
